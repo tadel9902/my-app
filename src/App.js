07deb860160eaa18863a78992b1300f9ff7470f3
tadel9902 <misery0499@gmail.com>
2024-06-13 16:29:07 +0700

    vailon

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React AD
+          Learn React AD dumb
         </a>
       </header>
     </div>
